b4edaa87572afef6ab4c00c5dd2abf243e0b748f
adolfosonchiche <sonadolfo36@gmail.com>
2021-10-14 20:08:51 -0600

    formulario para crear usuario nuevo

---
+++ Proyecto1TS1/src/main/webapp/js/validation.js
@@ -0,0 +1,24 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+function verOcultar() {
+    var tipo = document.getElementById("password1");
+    if (tipo.type === "password") {
+        tipo.type = "text";
+    } else {
+        tipo.type = "password";
+    }
+}
+
+function confirmarPassword() {
+    var tipo = document.getElementById("password-confirm");
+    if (tipo.type === "password") {
+        tipo.type = "text";
+    } else {
+        tipo.type = "password";
+    }
+}
---
+++ Proyecto1TS1/src/main/webapp/jsp/crear-usuario.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : crear-usuario
+    Created on : 14 oct. 2021, 18:25:00
+    Author     : hectoradolfo
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <script src="../js/validation.js"></script>
+        <title>JSP Page</title>
+    </head>
+    <body background="../resources/img/create_user.jpg">
+        
+        <%@include file="../html/crear_usuario/crear-usuario.html"%>
+        
+    </body>
+</html>
