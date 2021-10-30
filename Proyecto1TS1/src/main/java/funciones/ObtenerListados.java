b908a779d3e1eec017bdd9c94cd703e1d33bd0e2
Grifiun <denii.grifon@gmail.com>
2021-10-29 22:57:28 -0600

    se crearon las clases necesarias y los querys necesarios para obtener listados, a excepcion de obtener los dias del calendario (especificados en las consideraciones 3 FE y BE) ;

--- Proyecto1TS1/src/main/java/funciones/ObtenerListados.java
+++ Proyecto1TS1/src/main/java/funciones/ObtenerListados.java
@@ -5,15 +5,26 @@
 package funciones;
 
 import conection_db.Consultar;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  *
  * @author grifiun
  */
 public class ObtenerListados {
+    
+    /**
+     * Funcion para obtener datos de un listado que no necesitan restricciones, 
+     * es decir where
+     * @param tipoListado
+     * @return 
+     */
     public ArrayList<String> getFrom(String tipoListado){
         Consultar consultarDB = new Consultar();
+        ArrayList<String> datosWhere = new ArrayList<String>();
         
         String query;//obtenemos la query correspondiente  
         
@@ -27,10 +38,79 @@ public class ObtenerListados {
             case "fase_lunar":
                     query = "SELECT fase FROM FASE;";
                 break;
+            case "admin":
+                    query = "SELECT codigo_admin, nombre, correo FROM ADMIN;";
+                break;
+            case "usuario":
+                    query = "SELECT codigo_usuario, nombre, apellido, sexo, birth FROM USUARIO;";
+                break;
+            case "planta-admin":
+                    query = "SELECT nombre, tipo_planta, descripcion_planta, duracion_dias FROM PLANTA;";
+                break;
+            case "tipo-planta-admin":
+                    query = "SELECT tipo, descripcion FROM TIPOPLANTA;";
+                break;
             default:
                 return new ArrayList();       
         }
         
-        return consultarDB.obtenerListadoSimpleRegistros(query, new ArrayList<String>());
+        return consultarDB.obtenerListadoSimpleRegistros(query, datosWhere);
+    }
+    
+    /**
+     * Funcion para obtener datos propios de un usuario correspondiente
+     * @param tipoListado
+     * @param request
+     * @return 
+     */
+    public ArrayList<String> getFrom(String tipoListado, HttpServletRequest request){
+        GetAttributeParameterRequest obtenerParam = new GetAttributeParameterRequest(request);
+        Consultar consultarDB = new Consultar();
+        ArrayList<String> datosWhere = new ArrayList<String>();
+        
+        String usuario = "user";
+        try{
+            usuario = (String) request.getSession().getAttribute("codigo_usuario");
+        }catch(Error ex){
+        
+        }
+        
+        String query;//obtenemos la query correspondiente  
+        
+        switch(tipoListado){
+            case "contacto telefono":
+                    query = "SELECT codigo_telefono, telefono FROM CONTACTO_TELEFONO WHERE codigo_usuario = ?;";    
+                    datosWhere.add(usuario);//asignamos dato
+                break;
+            case "contacto correo":
+                    query = "SELECT codigo_correo, telefono FROM CONTACTO_CORREO WHERE codigo_usuario = ?;";    
+                    datosWhere.add(usuario);//asignamos dato
+                break;
+            case "siembras":
+                    query = "SELECT codigo_siembra, nombre_planta, fecha, estado FROM SIEMBRA WHERE codigo_usuario = ?;";
+                    datosWhere.add(usuario);//asignamos dato
+                break;
+            case "agenda hoy":
+                    query = "SELECT codigo_agenda, codigo_fecha, codigo_siembra, descripcion_agenda FROM AGENDA WHERE codigo_usuario = ? AND codigo_fecha = ?;";
+                    datosWhere.add(usuario);//asignamos dato
+                    
+                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+                    String fechaAuxiliar = df.format(new Date());
+                    datosWhere.add(fechaAuxiliar);
+                break;
+            case "agenda todos":
+                    query = "SELECT codigo_agenda, codigo_fecha, codigo_siembra, descripcion_agenda FROM AGENDA WHERE codigo_usuario = ?;";
+                    datosWhere.add(usuario);//asignamos dato                    
+                break;
+            case "agenda x":
+                    query = "SELECT codigo_agenda, codigo_fecha, codigo_siembra, descripcion_agenda FROM AGENDA WHERE codigo_usuario = ? AND codigo_fecha = ?;";
+                    datosWhere.add(usuario);//asignamos dato
+                    datosWhere.add(obtenerParam.getAttributOrParameter("fecha"));
+                break;
+            default:
+                return new ArrayList();       
+        }
+        
+        return consultarDB.obtenerListadoSimpleRegistros(query, datosWhere);
     }
 }
---
+++ Proyecto1TS1/src/main/java/funciones/ObtenerMes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package funciones;
+
+import entidades.Dia;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author grifiun
+ */
+public class ObtenerMes {
+    public List<ArrayList<Dia>> obtener(){
+        //solo declaracion del metodo
+        return null;
+    }
+}
