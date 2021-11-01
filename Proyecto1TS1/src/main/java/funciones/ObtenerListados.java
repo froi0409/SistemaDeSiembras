3e259a0d7854e9acdca52f000baed6d1ab898676
Grifiun <denii.grifon@gmail.com>
2021-10-31 20:28:01 -0600

    se corrigo la obtencion de informacion para varias querys, ahora retorna tablas de informacion (List de ArrayList de String) cuando se necesiten varios atributos por registro

--- Proyecto1TS1/src/main/java/funciones/ObtenerListados.java
+++ Proyecto1TS1/src/main/java/funciones/ObtenerListados.java
@@ -8,6 +8,7 @@ import conection_db.Consultar;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -58,12 +59,54 @@ public class ObtenerListados {
     }
     
     /**
+     * Funcion para obtener datos de un listado que no necesitan restricciones, 
+     * es decir where
+     * @param tipoListado
+     * @return 
+     */
+    public List<ArrayList<String>> getTablaFrom(String tipoListado){
+        Consultar consultarDB = new Consultar();
+        ArrayList<String> datosWhere = new ArrayList();
+        
+        String query;//obtenemos la query correspondiente  
+        
+        switch(tipoListado){
+           /* case "planta":
+                    query = "SELECT nombre FROM PLANTA;";
+                break;
+            case "tipo_planta":
+                    query = "SELECT tipo FROM TIPOPLANTA;";
+                break;
+            case "fase_lunar":
+                    query = "SELECT fase FROM FASE;";
+                break;*/
+            case "admin":
+                    query = "SELECT codigo_admin, nombre, correo FROM ADMIN;";
+                break;
+            case "usuario":
+                    query = "SELECT codigo_usuario, nombre, apellido, sexo, birth FROM USUARIO;";
+                break;
+            case "planta-admin":
+                    query = "SELECT nombre, tipo_planta, descripcion_planta, duracion_dias FROM PLANTA;";
+                break;
+            case "tipo-planta-admin":
+                    query = "SELECT tipo, descripcion FROM TIPOPLANTA;";
+                break;
+            default:
+                return new ArrayList();       
+        }
+        
+        return consultarDB.obtenerRegistros(query, datosWhere);
+    }
+    
+    
+    /**
      * Funcion para obtener datos propios de un usuario correspondiente
      * @param tipoListado
      * @param request
      * @return 
      */
-    public ArrayList<String> getFrom(String tipoListado, HttpServletRequest request){
+    public List<ArrayList<String>> getFrom(String tipoListado, HttpServletRequest request){
         GetAttributeParameterRequest obtenerParam = new GetAttributeParameterRequest(request);
         Consultar consultarDB = new Consultar();
         ArrayList<String> datosWhere = new ArrayList<String>();
@@ -111,6 +154,6 @@ public class ObtenerListados {
                 return new ArrayList();       
         }
         
-        return consultarDB.obtenerListadoSimpleRegistros(query, datosWhere);
+        return consultarDB.obtenerRegistros(query, datosWhere);
     }
 }
